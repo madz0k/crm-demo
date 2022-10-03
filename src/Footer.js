936cd8b18eb90861dace81d310267e17146ae669
Nikolay Bayborodin Personal GitHub Profile <bayborodin@proton.me>
2022-10-04 09:38:55 +1000

    Add the main template

--- src/Footer.js
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return <div>(c) 2022, Nikolay Bayborodin</div>;
-};
-
-export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="top_navbar">
+                <div className='user-info'>John Doe</div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
